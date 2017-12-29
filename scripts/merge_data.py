77c9e14e7af9c287a90967da316ae83ecf2d4ab2
David Eads <davideads@gmail.com>
2017-12-28 20:48:12 -0600

    switch to mapbox

---
+++ scripts/merge_data.py
@@ -0,0 +1,19 @@
+import json
+import random
+
+def merge_data():
+    with open('mx-geojson.json') as f:
+        data = json.load(f)
+        for feature in data['features']:
+            mun_code = feature['properties'].get('mun_code')
+            if mun_code:
+                feature['properties']['2013_fosas'] = random.randint(0, 50)
+                feature['properties']['2014_fosas'] = random.randint(0, 50)
+                feature['properties']['2015_fosas'] = random.randint(0, 50)
+                feature['properties']['2016_fosas'] = random.randint(0, 50)
+
+    with open('mx-geojson-merged.json', 'w') as f:
+        json.dump(data, f)
+
+if __name__ == '__main__':
+    merge_data()
