8cc189da38dd2482f0c3b216897a4b328dfcb32b
David Eads <davideads@gmail.com>
2018-11-11 12:20:27 -0600

    better table of municipalities

--- src/components/MunicipioRank.js
+++ src/components/MunicipioRank.js
@@ -3,33 +3,39 @@ import map from 'lodash/map';
 import sortBy from 'lodash/sortBy';
 import uniqBy from 'lodash/uniqBy';
 
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+
 class MunicipioRank extends React.Component {
   render() {
     const { municipioData, selectedYear, selectedVar } = this.props;
     const selected = (selectedYear == 2005) ? 'cumulative_2016' : selectedYear;
     const municipios = sortBy(uniqBy(map(municipioData, 'properties'), 'nom_mun'), '' + selectedVar + '_' + selected).reverse();
-
-    return (<table>
-      <thead>
-        <tr>
-          <th colSpan={2}></th>
-          <th className="number">Fosas</th>
-          <th className="number">Cuerpos</th>
-        </tr>
-      </thead>
-      <tbody>
-        {municipios.slice(0, 5).map( (m, i) => (
-          <tr key={"municipio" + i}>
-            <td>{i + 1}.</td>
-            <td className="municipio-name">{m.nom_mun}</td>
-            <td className="number">{m['fosas_' + selected]}</td>
-            <td className="number">{m['cuerpos_' + selected]}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>);
+    return (
+       <ReactTable
+        data={municipios}
+        columns={[
+          {
+            Header: "Municipio",
+            accessor: "nom_mun",
+          },
+          {
+            Header: "Fosas",
+            accessor: 'fosas_' + selected,
+            width: 60,
+          },
+          {
+            Header: "Cuerpos",
+            accessor: 'cuerpos_' + selected,
+            width: 60,
+          },
+        ]}
+        showPagination={false}
+        sortable={false}
+        pageSize={municipios.length}
+       />
+    );
   }
 }
 
 export default MunicipioRank;
-
